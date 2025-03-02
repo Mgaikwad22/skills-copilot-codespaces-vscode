9b003f595ef6f548c7dbfd8b8a03b1825400917a
Mgaikwad22 <108661607+Mgaikwad22@users.noreply.github.com>
2025-03-02 10:43:00 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
 function skillMember() {
-    console.log('skillMember');
+    return{
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    }
 }
