673e6c0f1ea6c3dc343de215f551b0e64ed1b427
Mgaikwad22 <108661607+Mgaikwad22@users.noreply.github.com>
2025-03-02 10:37:40 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,3 @@
 function skillMember() {
-    this.name = 'Member';
-    this.id = 'member';
-    this.type = 'skill';
-    this.description = 'Member description';
+    console.log('skillMember');
 }
